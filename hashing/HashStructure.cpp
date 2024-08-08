a1589a4ae85dff06311b658891922e7bd7a2dab0
sheikhfahadshaon <fahadshaon09@gmail.com>
2024-08-08 23:46:10 +0600

    StructHash added

---
+++ hashing/HashStructure.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class StringHash
+{
+    int p1 = 137;
+    int mod1 = 127657753;
+    int p2 = 277;
+    int mod2 = 987654319;
+    int N;
+    vector<pair<int, int>> pw, ipw, pref;
+    int invp1, invp2;
+
+    int power(long long n, long long k, int mod)
+    {
+        int ans = 1 % mod;
+        n %= mod;
+        if (n < 0)
+            n += mod;
+        while (k)
+        {
+            if (k & 1)
+                ans = (long long)ans * n % mod;
+            n = (long long)n * n % mod;
+            k >>= 1;
+        }
+        return ans;
+    }
+
+    void prec()
+    {
+        pw[0] = {1, 1};
+        for (int i = 1; i < N; i++)
+        {
+            pw[i].first = 1LL * pw[i - 1].first * p1 % mod1;
+            pw[i].second = 1LL * pw[i - 1].second * p2 % mod2;
+        }
+
+        invp1 = power(p1, mod1 - 2, mod1);
+        invp2 = power(p2, mod2 - 2, mod2);
+
+        ipw[0] = {1, 1};
+        for (int i = 1; i < N; i++)
+        {
+            ipw[i].first = 1LL * ipw[i - 1].first * invp1 % mod1;
+            ipw[i].second = 1LL * ipw[i - 1].second * invp2 % mod2;
+        }
+    }
+
+    void build(string s)
+    {
+        int n = s.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            pref[i].first = 1LL * s[i] * pw[i].first % mod1;
+            if (i)
+                (pref[i].first += pref[i - 1].first) %= mod1;
+
+            pref[i].second = 1LL * s[i] * pw[i].second % mod2;
+            if (i)
+                (pref[i].second += pref[i - 1].second) %= mod2;
+        }
+    }
+
+public:
+    StringHash(string s)
+    {
+        N = s.length() + 10;
+        pw.assign(N, {0, 0});
+        ipw.assign(N, {0, 0});
+        pref.assign(N, {0, 0});
+        prec();
+        build(s);
+    }
+
+    pair<int, int> get_hash(int i, int j)
+    {
+        assert(i <= j);
+
+        pair<int, int> hs({0, 0});
+
+        hs.first = pref[j].first;
+        if (i)
+            hs.first = (hs.first - pref[i - 1].first + mod1) % mod1;
+        hs.first = 1LL * hs.first * ipw[i].first % mod1;
+
+        hs.second = pref[j].second;
+        if (i)
+            hs.second = (hs.second - pref[i - 1].second + mod2) % mod2;
+        hs.second = 1LL * hs.second * ipw[i].second % mod2;
+
+        return hs;
+    }
+};
